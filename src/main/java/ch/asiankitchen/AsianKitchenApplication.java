d67da584c94f1be26b8dc63dcbe2d8d7afdf6f4d
kaama07 <tsering.anodunkhartsang@edu.tbz.ch>
2025-06-05 18:35:28 +0200

    Initial commit: Asian Kitchen backend

---
+++ src/main/java/ch/asiankitchen/AsianKitchenApplication.java
@@ -0,0 +1,13 @@
+package ch.asiankitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsianKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsianKitchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/asiankitchen/AsianKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.asiankitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsianKitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
