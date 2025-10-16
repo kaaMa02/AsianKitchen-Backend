f39ebbb575da07d57d10cbee4a267c442350f85b
kaaMa02 <karmatselha123@icloud.com>
2025-10-16 16:13:13 +0200

    updated to the better version

---
+++ src/main/resources/db/migration/V21__delete_test_data.sql
@@ -0,0 +1,20 @@
+DELETE FROM public.orders
+WHERE id='8f52d4cd-fc0a-45df-988d-0069bb71bc46'::uuid;
+DELETE FROM public.orders
+WHERE id='18f9704d-183e-4c92-9116-e99a8398d8b1'::uuid;
+DELETE FROM public.orders
+WHERE id='b7816425-c764-437a-9a04-1ac61eee8ac4'::uuid;
+DELETE FROM public.orders
+WHERE id='a54f6478-0cac-4607-b1b7-f9d9d5a22032'::uuid;
+DELETE FROM public.orders
+WHERE id='35c8d3c2-5fab-4259-9152-6dbf1584cfa2'::uuid;
+DELETE FROM public.orders
+WHERE id='76fdf4bc-dcea-41a7-a526-a785e953131f'::uuid;
+DELETE FROM public.orders
+WHERE id='c1237b51-fba7-4e6f-aee9-0de30af3ba39'::uuid;
+DELETE FROM public.orders
+WHERE id='1b5857d9-4d89-43c4-a101-bbb720bfc4cf'::uuid;
+DELETE FROM public.orders
+WHERE id='b75fb260-0a1d-4c18-9edd-725c403dc2cc'::uuid;
+DELETE FROM public.orders
+WHERE id='697adddc-3471-419a-b371-616cf3d4418a'::uuid;
