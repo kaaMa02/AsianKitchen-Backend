a81b6455b79ef717544434bf76562a286232a365
kaaMa02 <karmatselha123@icloud.com>
2025-10-21 18:19:45 +0200

    deleted test datas

---
+++ src/main/resources/db/migration/V25__delete_test_datas.sql
@@ -0,0 +1,24 @@
+DELETE FROM public.orders
+WHERE id='7518d923-eef3-498e-be9f-a25288d50ada'::uuid;
+DELETE FROM public.orders
+WHERE id='98997017-6549-44f5-9a88-e9df5d30741e'::uuid;
+DELETE FROM public.orders
+WHERE id='e002d6c7-dcff-4ab0-99d7-7db6f1f6b211'::uuid;
+DELETE FROM public.orders
+WHERE id='571d9fc5-ae13-45ea-93c1-7cd5bd32bfe1'::uuid;
+DELETE FROM public.orders
+WHERE id='dbebaae2-0f6a-4e48-b457-5c75517a3a90'::uuid;
+DELETE FROM public.orders
+WHERE id='57951fa0-88ef-4b32-90d4-ad47de2461f8'::uuid;
+DELETE FROM public.orders
+WHERE id='6ea678d6-ac24-494a-b396-97732106a102'::uuid;
+DELETE FROM public.orders
+WHERE id='5b213552-1706-4035-86cc-7e255d2fe36b'::uuid;
+DELETE FROM public.orders
+WHERE id='12288d1c-48ca-43aa-b46e-39667a0c430d'::uuid;
+DELETE FROM public.orders
+WHERE id='c9cff6fc-e066-4623-a593-eb89a21e9e5d'::uuid;
+DELETE FROM public.orders
+WHERE id='94e0a50d-e411-4791-a3ea-42af61cd130d'::uuid;
+DELETE FROM public.orders
+WHERE id='3d78587c-57e7-4d38-a1ff-41b24fd30cc6'::uuid;
