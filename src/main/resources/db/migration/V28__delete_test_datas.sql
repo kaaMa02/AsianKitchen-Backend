4203ff56e08a04a30bc09781955a6f729e3d6b7a
kaaMa02 <karmatselha123@icloud.com>
2025-11-12 11:43:50 +0100

    delete test datas

---
+++ src/main/resources/db/migration/V28__delete_test_datas.sql
@@ -0,0 +1,6 @@
+DELETE FROM public.orders WHERE id='61add8b5-2eac-434c-8811-df2655de3184'::uuid;
+DELETE FROM public.orders WHERE id='6f10aed3-d2f6-49bd-9ad7-549e07a259a9'::uuid;
+DELETE FROM public.orders WHERE id='60508ab8-9bd2-470f-84a3-8814f9d7fb3c'::uuid;
+DELETE FROM public.orders WHERE id='ee5603ae-3296-4994-8a1b-1aca88c5d4b2'::uuid;
+DELETE FROM public.orders WHERE id='e8067658-99c0-45f7-8fe4-e6a431fda298'::uuid;
+DELETE FROM public.orders WHERE id='278ff1c9-93b3-4a84-9a23-4546859cebf5'::uuid;
